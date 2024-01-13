24d9d2f6c7a3eb256ccc640ae7ae414f73f793d0
“shamnad” <hamzashamnad123@gmail.com>
2024-01-13 21:20:42 +0530

    adding data via react

--- firebase/src/App.js
+++ firebase/src/App.js
@@ -14,21 +14,46 @@ import { firebaseApp,firestore} from "./firebase/config";
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Fetch data from firebase firestore DB</h1>
-      <button onClick={()=>{
-        console.log('hiiii')
-        firebaseApp.firestore().collection('products ').get().then(snapshot => {
-          snapshot.forEach((obj) => {
-              console.log(obj.data())
-            })
-        })
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Fetch data from firebase firestore DB</h1>
+//       <button onClick={()=>{
+//         console.log('hiiii')
+//         firebaseApp.firestore().collection('products ').get().then(snapshot => {
+//           snapshot.forEach((obj) => {
+//               console.log(obj.data())
+//             })
+//         })
 
-      }}>click here</button>
-    </div>
-  );
-}
+//       }}>click here</button>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+
+
+
+// ADDING DATA
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Fetch data from firebase firestore DB</h1>
+//       <button onClick={()=>{
+//         console.log('hiiii')
+//         firebaseApp.firestore().collection('products ').add({
+//           name:'google',
+//           price:50000,
+//           model:'pixel 4a'
+//         })
+      
+
+//       }}>click here</button>
+//     </div>
+//   );
+// }
+
+// export default App;
